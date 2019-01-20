09bd94dc24d0bf25fc3c25042d1b78c58b58fc64
Afonso Silva <ajcerejeira@gmail.com>
2019-01-20 12:38:02 +0000

    Created login page

---
+++ src/pages/accident-list/accident-list.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AccidentListPage } from './accident-list';
+
+@NgModule({
+  declarations: [
+    AccidentListPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AccidentListPage),
+  ],
+})
+export class AccidentListPageModule {}
