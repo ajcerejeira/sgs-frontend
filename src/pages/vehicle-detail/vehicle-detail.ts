8aacb64b9718bc1404f8010bf639bd78894c7052
Afonso Silva <ajcerejeira@gmail.com>
2019-01-21 19:50:08 +0000

    Updated camera image return value

--- src/pages/vehicle-detail/vehicle-detail.ts
+++ src/pages/vehicle-detail/vehicle-detail.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { DomSanitizer } from "@angular/platform-browser";
+import { DomSanitizer, SafeUrl } from "@angular/platform-browser";
 import { IonicPage, NavController, NavParams, AlertController } from "ionic-angular";
 import { Camera, CameraOptions } from '@ionic-native/camera';
 
@@ -16,7 +16,7 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
   templateUrl: "vehicle-detail.html"
 })
 export class VehicleDetailPage {
-  pictures: string[] = [];
+  pictures: SafeUrl[] = [];
   vehiclePage: string = 'info'; // Default segment to load
   constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, private camera: Camera, private domSanitizer: DomSanitizer) {}
 
@@ -53,7 +53,7 @@ export class VehicleDetailPage {
     this.camera.getPicture(options).then((imageData) => {
       // imageData is either a base64 encoded string or a file URI
       // If it's base64 (DATA_URL):
-      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      let base64Image = this.domSanitizer.bypassSecurityTrustUrl('data:image/jpeg;base64,' + imageData);
       this.pictures.push(base64Image);
       console.log(this.pictures);
      }, (err) => {
