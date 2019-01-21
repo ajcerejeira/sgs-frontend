ad6881eb48d07d9ddad2ab93283ebceb96da3c7b
Afonso Silva <ajcerejeira@gmail.com>
2019-01-21 15:21:11 +0000

    Added Accident delete confirmation prompt

--- src/pages/accident-info/accident-info.ts
+++ src/pages/accident-info/accident-info.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, PopoverController, ViewController, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the AccidentInfoPage page.
@@ -14,12 +14,35 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'accident-info.html',
 })
 export class AccidentInfoPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AccidentInfoPage');
   }
 
+  confirmDelete() {
+    const prompt = this.alertCtrl.create({
+      title: 'Tem a certeza que pretende remover este sinistro?',
+      message: 'Esta ação é irreversível. Todos os dados relativos a este sinistro serão apagados',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+        },
+        {
+          text: 'Apagar',
+        },
+      ]
+    });
+    prompt.present();
+  }
+
+  showReport() {
+    const alert = this.alertCtrl.create({
+      title: 'Auto do sinistro',
+      subTitle: 'Este popup desaparecerá e será mostrado o PDF do auto',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
 }
