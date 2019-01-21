8755c34cb2f5684ec60d8c745079ffb416316fc0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-21 18:34:55 +0000

    Deploy ajcerejeira/sgs-frontend to github.com/ajcerejeira/sgs-frontend.git:gh-pages

--- cordova_plugins.js
+++ cordova_plugins.js
@@ -362,6 +362,36 @@ module.exports = [
         "id": "cordova-plugin-googlemaps.PluginEnvironment",
         "pluginId": "cordova-plugin-googlemaps",
         "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
+        "id": "cordova-plugin-camera.Camera",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "Camera"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
+        "id": "cordova-plugin-camera.CameraPopoverOptions",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "CameraPopoverOptions"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/Camera.js",
+        "id": "cordova-plugin-camera.camera",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "navigator.camera"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/src/browser/CameraProxy.js",
+        "id": "cordova-plugin-camera.CameraProxy",
+        "pluginId": "cordova-plugin-camera",
+        "runs": true
     }
 ];
 module.exports.metadata = 
@@ -374,7 +404,8 @@ module.exports.metadata =
     "cordova-plugin-ionic-webview": "2.3.2",
     "cordova-plugin-ionic-keyboard": "2.1.3",
     "cordova-plugin-googlemaps": "2.4.6",
-    "cordova-plugin-geolocation": "4.0.1"
+    "cordova-plugin-geolocation": "4.0.1",
+    "cordova-plugin-camera": "4.1.0-dev"
 }
 // BOTTOM OF METADATA
 });
