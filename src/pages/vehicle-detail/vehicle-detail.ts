21b431e5b06bb3d962b1f95576f060a7627a41fc
Afonso Silva <ajcerejeira@gmail.com>
2019-01-22 17:53:58 +0000

    Fixed android photo storage

--- src/pages/vehicle-detail/vehicle-detail.ts
+++ src/pages/vehicle-detail/vehicle-detail.ts
@@ -90,7 +90,7 @@ export class VehicleDetailPage {
   openCamera() {
     const options: CameraOptions = {
       quality: 100,
-      destinationType: this.camera.DestinationType.FILE_URI,
+      destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE,
     };
