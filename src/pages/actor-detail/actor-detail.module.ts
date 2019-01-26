770ae5d442419e9944c27c277aead9189d401de6
JoaoMVieira <33961226+JoaoMVieira@users.noreply.github.com>
2019-01-26 12:41:10 +0000

    Add: Signature (missing resize)
    Added signature to top left corner of signature page. Needs resize function

--- src/pages/actor-detail/actor-detail.module.ts
+++ src/pages/actor-detail/actor-detail.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ActorDetailPage } from './actor-detail';
-
+import { SignaturePadModule } from 'angular2-signaturepad';
 @NgModule({
   declarations: [
     ActorDetailPage,
+    
   ],
   imports: [
     IonicPageModule.forChild(ActorDetailPage),
+    SignaturePadModule
+    
   ],
+  
 })
 export class ActorDetailPageModule {}
--- src/pages/actor-detail/actor-detail.ts
+++ src/pages/actor-detail/actor-detail.ts
@@ -1,11 +1,12 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 import {
   IonicPage,
   NavController,
   NavParams,
+  ModalController,
   AlertController
 } from "ionic-angular";
-
+import {SignaturePad} from 'angular2-signaturepad/signature-pad';
 /**
  * Generated class for the ActorDetailPage page.
  *
@@ -19,7 +20,19 @@ import {
   templateUrl: "actor-detail.html"
 })
 export class ActorDetailPage {
+  @ViewChild(SignaturePad) public signaturePad : SignaturePad;
+
+  public signaturePadOptions : Object = {
+    'minWidth': 2,
+    'canvasWidth': 340,
+    'canvasHeight': 200
+  };
+  public signatureImage : string;
+  public drawn = false;
+  public width = 340;
+  public height = 200;
   actorPage: string = "info"; // Default segment to load
+  
   testimonials: any[] = [
     {
       id: "12345678",
@@ -34,11 +47,21 @@ export class ActorDetailPage {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
+    public modalController:ModalController,
     public alertCtrl: AlertController
   ) {}
 
+  canvasResize(){
+    let canvas = document.querySelector('canvas');
+    //this.signaturePad.set('minWidth', 1);
+    //this.signaturePad.set('canvasWidth', canvas.offsetWidth);
+    //this.signaturePad.set('canvasHeight', canvas.offsetHeight);
+    
+  }
+
   ionViewDidLoad() {
     console.log("ionViewDidLoad ActorDetailPage");
+    this.canvasResize();
   }
 
   confirmDelete() {
@@ -58,6 +81,24 @@ export class ActorDetailPage {
     });
     prompt.present();
   }
+
+  
+
+  openSignatureModel(){
+    //let modal = this.modalController.create('ActorSignaturePage');
+    //modal.present();
+    if(this.drawn) {this.signatureImage = null; this.drawn = false;}
+    else{
+      if(this.signaturePad.isEmpty() == false){
+      this.drawn = true;
+      this.signatureImage = this.signaturePad.toDataURL();
+      this.signaturePad.clear();
+    }
+    }
+  
+  }
+
+  
 
   actorEdit() {
     const prompt = this.alertCtrl.create({
