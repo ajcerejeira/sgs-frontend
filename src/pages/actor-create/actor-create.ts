07d4e04e8e7cd96676fc7b499f6d37c3c2ec968d
humbertovaz <humbertojoao13@hotmail.com>
2019-01-29 16:42:16 +0000

    Refactor actor-create and actor-detail (new api)

--- src/pages/actor-create/actor-create.ts
+++ src/pages/actor-create/actor-create.ts
@@ -23,36 +23,37 @@ export class ActorCreatePage {
   public actorType: string[];
   public identDocument: string[];
   public injuryType : string[];
-  public url : string = "http://sgs-backend.herokuapp.com/api/actors/";
   public vehicles: any;
+  public register: any;
   public registers: any;
   id: string;
-  public vehicle: any;
+  public url : string = "http://sgs-backend.herokuapp.com/api/accidents/"+this.id+"/actors";
   public vehicleC: any;
   public birth: any;
   public expires: any;
   
   constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder, public http: Http, public toastCtrl: ToastController) {
     this.formGroup = this.formBuilder.group({
+      identityDocumentType: [null, []],
+      identityDocumentNumber: [null, Validators.required],
+      identityDocumentExpirationDate: [null, []],
+      identityDocumentEmitedBy: [null, Validators.required],
       name: [null, Validators.required],
       birth: [null, []],
-      phone: [null, Validators.required],
       email: [null, [Validators.required, Validators.email]],
+      phone: [null, Validators.required],
       nationality: [null, Validators.required],
       naturality: [null, Validators.required],
-      locality: [null, Validators.required],
-      zipcode: [null, Validators.required],
-      address: [null, Validators.required],
-      doorNumber: [null, Validators.required],
-      idType: [null, []],
-      idNumber: [null, Validators.required],
       parentage: [null, Validators.required],
-      emitedBy: [null, Validators.required],
-      expires: [null, []],
+      parentage1: [null, Validators.required],
+      locality: [null, Validators.required],
+      address: [null, Validators.required],
+      zipcode: [null, Validators.required],
+      doorNumber: [null, Validators.required],
       alcoholTest: [null, Validators.required],
       vehicle: [null, Validators.required],
-      injury:  [null, []],
       role: [null, Validators.required],
+      wounds:  [null, []],
 
     });
     this.id = navParams.get('id');
@@ -68,38 +69,38 @@ export class ActorCreatePage {
   }
   
   createActor(id){
-    let postData = {
-      "name": this.formGroup.value["name"],
-      "documentNumber": this.formGroup.value["idNumber"],
-    }
     var birthD = new Date(this.birth);
     var expiresD = new Date(this.expires);
-    
-    var actor = {
+    const parentageList = [];
+    if (this.formGroup.value["parentage"].length>0) parentageList.push(this.formGroup.value["parentage"]);
+    if (this.formGroup.value["parentage1"].length>0) parentageList.push(this.formGroup.value["parentage1"]);
 
+    var person = {
+      identityDocumentType: this.formGroup.value["identityDocumentType"],
+      identityDocumentNumber: this.formGroup.value["identityDocumentNumber"],
+      identityDocumentExpirationDate: expiresD,
+      identityDocumentEmitedBy: this.formGroup.value["identityDocumentEmitedBy"],
       name: this.formGroup.value["name"],
       birth: birthD, 
-      phone: this.formGroup.value["phone"],
       email: this.formGroup.value["email"],
+      phone: this.formGroup.value["phone"],
       nationality: this.formGroup.value["nationality"],
       naturality: this.formGroup.value["naturality"],
+      parentage: parentageList,
       locality: this.formGroup.value["locality"],
-      zipcode: this.formGroup.value["zipcode"],
       address: this.formGroup.value["address"],
+      zipcode: this.formGroup.value["zipcode"],
       doorNumber: this.formGroup.value["doorNumber"],
-      accident: this.id,
-      idType: this.formGroup.value["idType"],
-      idNumber: this.formGroup.value["idNumber"],
-      parentage: [this.formGroup.value["parentage"]],
-      emitedBy: this.formGroup.value["emitedBy"],
-      expires: expiresD,
-      alcoholTest: Number(this.formGroup.value["alcoholTest"]),
-      vehicle: this.getCarId(this.vehicle),
-      injury: this.formGroup.value["injury"],
-      role: this.formGroup.value["role"],
-
     }
-    this.http.post(this.url, actor)
+
+    var personDetails = {
+      person,
+      role: this.formGroup.value["role"],
+      wounds: this.formGroup.value["wounds"],
+      alcoholTest: parseFloat(this.formGroup.value["alcoholTest"]),
+      id: this.getCarId(this.register)
+    }
+    this.http.post("http://sgs-backend.herokuapp.com/api/accidents/"+this.id+"/actors", personDetails)
       .subscribe(data => {
         console.log(data['_body']);
         const toast = this.toastCtrl.create({position: 'top', message: "Interveniente criado com sucesso!", duration: 3000 });
@@ -113,27 +114,32 @@ export class ActorCreatePage {
   }
   // MATRICULAS
   getRegisters(){
-    for(let i = 0; i < this.vehicles.length; i++){
+    for(const vehicle of this.vehicles) {
+      //this.registers.push(vehicle.meta.register);
       this.vehicleC={
-                   "register" :this.vehicles[i].register,
-                  }
+        "register" :vehicle.meta.register,
+       }
       this.registers.push(this.vehicleC);
     }
   }
 
   getCarId(register){
+    // for(const vehicle of this.vehicles){
+    //     if (vehicle.meta.register === register)
+    //       return vehicle.id; // Este campo existe?
+    // }
     for(let i=0; i<this.vehicles.length; i++){
-      if (this.vehicles[i].register===register){
+      if (this.vehicles[i].meta.register===register){
         return this.vehicles[i].id;
       }
     }
   }
   
   async ionViewDidLoad() {
-    console.log("ID ACC:" +  this.id )
-    await this.http.get("https://sgs-backend.herokuapp.com/api/accidents/"+this.id).map(res => res.json()).subscribe(res => {
-      this.vehicles=res.vehicles;
-      this.getRegisters()
+    
+    await this.http.get("https://sgs-backend.herokuapp.com/api/accidents/"+this.id + '/vehicles').map(res => res.json()).subscribe(res => {
+    this.vehicles=res;
+    this.getRegisters()
     }, error => {
       console.log(error);
     });
