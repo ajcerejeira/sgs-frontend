7ef167be52aab3b5e48ff599d784b3c888dae49f
Afonso Silva <ajcerejeira@gmail.com>
2019-01-29 21:14:26 +0000

    Commented components.module.ts

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+/*import { NgModule } from '@angular/core';
 import { PinModulerComponent } from './pin-moduler/pin-moduler';
+
 @NgModule({
   declarations: [PinModulerComponent],
   imports: [],
   exports: [PinModulerComponent],
 })
 export class ComponentsModule {}
+*/
