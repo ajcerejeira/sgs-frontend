9a043ce27c57af938fb50a891907e2866d6bc3dd
JoaoMVieira <33961226+JoaoMVieira@users.noreply.github.com>
2019-01-30 14:50:05 +0000

    Fix: actor without role go to "other" category

--- src/pages/actor-list/actor-list.ts
+++ src/pages/actor-list/actor-list.ts
@@ -85,7 +85,7 @@ export class ActorListPage {
             if (actors[i].role === 'Witness') {
               this.witnesses.push(actors[i]);
             }
-            if (actors[i].role === 'Other') {
+            if (actors[i].role === 'Other' || !actors[i].role) {
               this.others.push(actors[i]);
             }
           }
